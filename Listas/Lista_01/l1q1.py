e6cb372b481fa0a7e52c100e80d5b179dd9d4118
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2018-07-06 20:19:38 -0300

    Add gabarito lista1

---
+++ Listas/Lista_01/l1q1.py
@@ -0,0 +1,15 @@
+# IFPB Campus João Pessoa
+# Curso Técnico em Informática Integrado ao Ensino Médio
+# Algoritmos de Lógica de Programação
+
+# Profa. Valéria Cavalcanti - 06/07/2018
+
+dividendo = int(input("Informe o dividendo: "))
+divisor = int(input("Informe o divisor: "))
+quociente = dividendo // divisor
+resto = dividendo % divisor
+
+print("Dividendo = " + str(dividendo))
+print("Divisor = " + str(divisor))
+print("Quociente = " + str(quociente))
+print("Resto = " + str(resto))
---
+++ Listas/Lista_01/l1q2.py
@@ -0,0 +1,15 @@
+# IFPB Campus João Pessoa
+# Curso Técnico em Informática Integrado ao Ensino Médio
+# Algoritmos de Lógica de Programação
+
+# Profa. Valéria Cavalcanti - 06/07/2018
+
+qtde_pasteis = int(input("Digite a quantidade de pastéis: "))
+qtde_refri = int(input("Digite a quantidade de refrigerantes: "))
+
+valor_pasteis = qtde_pasteis * 3.5
+valor_refri = qtde_refri * 2
+
+total = valor_pasteis + valor_refri
+
+print("Total = " + str(total))
